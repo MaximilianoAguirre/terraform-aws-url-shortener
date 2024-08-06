40ee694b996bc2b43b29dabc9cd8742d0fec520a
MaximilianoAguirre <maximiliano.aguirre@outlook.es>
2024-08-06 11:34:35 -0300

    add create url functionality in web
    Signed-off-by: MaximilianoAguirre <maximiliano.aguirre@outlook.es>

--- web/main.js
+++ web/main.js
@@ -5,6 +5,7 @@ const getURLs = () => {
             const urls = response.data;
 
             let table_content = document.getElementById("table-content");
+            table_content.innerHTML = '';
 
             urls.forEach(url => {
                 let new_url = document.createElement('tr');
@@ -13,6 +14,7 @@ const getURLs = () => {
                 let new_url_a = document.createElement('a');
                 new_url_a.innerText = url.id.S;
                 new_url_a.href = config.base_url + "/" + url.id.S
+                new_url_a.target = "_blank"
                 new_url_a.className = "link-light link-underline-dark"
                 new_url_id.appendChild(new_url_a)
 
@@ -30,3 +32,21 @@ const getURLs = () => {
 
 
 getURLs();
+
+const createURL = (e) => {
+    url_id = document.getElementById('url-id').value
+    url = document.getElementById('url').value
+
+
+
+    console.log(url_id, url)
+    axios
+        .post(config.base_url + "/url", {
+            id: url_id,
+            url: url
+        })
+        .then((response) => {
+            bootstrap.Modal.getInstance(document.getElementById("createUrl")).hide();
+            getURLs();
+        })
+}
